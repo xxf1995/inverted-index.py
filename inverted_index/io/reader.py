8e9093850f73e5e4157d2de7cbbbbbed8bb081af
wb <519369865@qq.com>
2017-05-16 23:16:01 +0200

    update reader tests

--- inverted_index/io/reader.py
+++ inverted_index/io/reader.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
 import string
 from os import listdir
 from os.path import isfile, join
@@ -5,76 +8,66 @@ from os.path import isfile, join
 
 class Reader:
 
-	def __init__(self):
-		self.dir = os.environ.get("DIRECTORY")
+    def __init__(self):
+        self.dir = os.environ.get('DIRECTORY')
 
-	def _get_docs(self):
-		"""Get all docs under directory, as docId
-        - Args:
-        - Returns:
-            docs: list of docs
-            num_docs: number of documents
-		"""
-		docs = [f
-		        for
-		        f
-		        in
-		        listdir(self.dir)
-		        if isfile(join(self.dir, f))]
-		return docs, len(docs)
+    def _get_docs(self):
+        """Get all docs under directory, as docId
+            - Args:
+            - Returns:
+                docs: list of docs
+               num_docs: number of documents
+            """
+        print type(os.environ.get("DIRECTORY"))
+        print self.dir
+        print type(self.dir)
+        docs = [f for f in listdir(self.dir) if isfile(join(self.dir,
+                f))]
+        return (docs, len(docs))
 
-	def _remove_punc(self, content):
-		"""Remove punctuation from given text"""
-        return content.translate(None,
-            string.punctuation)
+    def _remove_punc(self, content):
+        """Remove punctuation from given text"""
+
+        return content.translate(None, string.punctuation)
 
     def _read_doc(self, doc_path):
-    	"""Read a document, get all terms
-    	- Args:
-    	    doc_path: str of document path
-    	- Returns:
-    	    terms: terms within document as list
-    	"""
-    	with open(doc_path, 'r') as f:
-    		terms = self._remove_punc(f.read())
-    		return terms.split()
+        """Read a document, get all terms
+    ....    - Args:
+    ....        doc_path: str of document path
+    ....    - Returns:
+    ....        terms: terms within document as list
+    ....    """
+
+        with open(doc_path, 'r') as f:
+            terms = self._remove_punc(f.read())
+            return terms.split()
 
     def _make_runs(self, docs, num_docs):
-    	"""Make runs for memory saving
-        - Args:
-            docs: name of docs
-            num_docs: number of documents
-        - Returns:
-            runs: list of list of docs
+        """Make runs for memory saving
+            - Args:
+                docs: name of docs
+                num_docs: number of documents
+            - Returns:
+                runs: list of list of docs
                   e.g. [[doc1, doc2], [doc3, doc4]]
-    	"""
-    	num_runs = num_docs / 10 + 1
-    	runs = []
-    	for run in xrange(1, num_runs):
-    		# get current run of docs
-    		current_run = docs[: num_runs]
-    		# remove current run from docs
-    		docs = docs[num_runs:]
-    		# append to runs
-    		runs.append(current_run)
-    	return runs
+    ....    """
 
+        num_runs = num_docs / 10 + 1
+        runs = []
+        for run in xrange(1, num_runs):
 
+                # get current run of docs
 
+            current_run = docs[:num_runs]
 
+            # remove current run from docs
 
+            docs = docs[num_runs:]
 
+            # append to runs
 
-
-
-
-
-
-
-
-
-
-
+            runs.append(current_run)
+        return runs
 
 
 
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,15 +1,13 @@
+import os
 import unittest
 from inverted_index.io.reader import Reader
 
 class TestReader(unittest.TestCase):
 	"""Test reader module."""
+
 	def test_get_docs(self):
 		r = Reader()
-		# mock dirs
-		with mock.patch('Reader.isdir') as mocked_isdir, \
-		    mock.patch('Reader.listdir') as mocked_listdir:
-		    mocked_isdir.return_value = True
-		    mocked_listdir.return_value = ['filename1', 'filename2']
+		r.dir = os.path.dirname(os.path.realpath(__file__)) + '/test_docs/'
 		docs, num_docs = r._get_docs()
 		self.assertIsInstance(docs, list)
 		self.assertIsInstance(num_docs, int)
@@ -18,10 +16,9 @@ class TestReader(unittest.TestCase):
 		r = Reader()
 		test_str = "Hi, this is a test sentence."
 		test_str = r._remove_punc(test_str)
-		print test_str
-		self.assertEqual(test_str, 'Hi this is a test sentence.')
+		self.assertEqual(test_str, 'Hi this is a test sentence')
 
-	def test__read_doc(self):
+	def test_read_doc(self):
 		pass
 
 	def test_make_runs(self):
