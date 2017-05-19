9cad82ce10606968c4b383c4dd9c0e4abbaaf402
wb <519369865@qq.com>
2017-05-19 15:29:39 +0200

    finish io feature (#1)

---
+++ inverted_index/utils/mult_thread.py
@@ -0,0 +1,18 @@
+import itertools
+from multiprocessing import cpu_count
+from multiprocessing.dummy import Pool as ThreadPool
+
+def mlp(func, run):
+    """multithead_processing
+    - Args:
+        func: function for multithead_processing
+        run: list of documents
+    - Returns:
+        terms: list of terms in all runs
+    """
+    pool = ThreadPool(cpu_count())
+    terms = pool.map(func, run)
+    pool.close()
+    pool.join()
+    terms = list(itertools.chain.from_iterable(terms))
+    return terms
