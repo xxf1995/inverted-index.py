aaa7c3aed2c21558ea065204d90ac10fdfc7ee44
wb <519369865@qq.com>
2017-09-07 17:28:26 +0200

    update util

--- inverted_index/util.py
+++ inverted_index/util.py
@@ -6,16 +6,6 @@ import redis
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
-temp_dir = os.path.abspath(
-        os.path.join(
-            __file__,
-            os.pardir)) + '/temp/'
-
-def clear_temp():
-    """clear temp folder"""
-    files = [f for f in os.listdir(temp_dir)]
-    [os.remove(temp_dir + f) for f in files]
-
 
 def get_docs():
     """Get all docs under directory, as docId
@@ -27,27 +17,6 @@ def get_docs():
     docs = [file_dir + f for f in os.listdir(file_dir) if os.path.isfile(os.path.join(file_dir,
             f))]
     return docs
-
-def make_runs(docs):
-    """Make runs for memory saving
-    - Args:
-        docs: name of docs
-        num_docs: number of documents
-    - Returns:
-        runs: list of list of docs
-              e.g. [[doc1, doc2], [doc3, doc4]]
-    """
-    threshold = 200
-    num_runs = len(docs) / threshold + 1
-    runs = []
-    for run in xrange(0, num_runs):
-        # get current run of docs.
-        current_run = docs[:threshold - 1]
-        # remove current run from docs.
-        docs = docs[threshold:]
-        # append to runs.
-        runs.append(current_run)
-    return runs
 
 def json_writer(content):
     with open(''.join([temp_dir, 'dictionary.json']), 'w') as f:
